394da832c4cfb7de4b88823b176bc55f00bac935
carolinafm216 <39195248+carolinafm216@users.noreply.github.com>
2023-03-08 08:53:51 +0000

    assignment_1

---
+++ assignments/life_expectancy/__init__.py
@@ -0,0 +1,8 @@
+import argparse
+from life_expectancy.cleaning import clean_data
+
+LIFE_EXPECTANCY_FILE_PATH=r"C:\Users\cfmarreiros\Documents\GitHub\FAAST\assignments\life_expectancy\data\eu_life_expectancy_raw.tsv"
+SAVE_FILE_PATH=r"C:\Users\cfmarreiros\Documents\GitHub\FAAST\assignments\life_expectancy\data"
+
+if __name__ == "__main__":  # pragma: no cover
+    clean_data(LIFE_EXPECTANCY_FILE_PATH,'PT')
--- assignments/life_expectancy/tests/test_cleaning.py
+++ assignments/life_expectancy/tests/test_cleaning.py
@@ -1,13 +1,15 @@
 """Tests for the cleaning module"""
 import pandas as pd
+import argparse
 
 from life_expectancy.cleaning import clean_data
-from . import OUTPUT_DIR
+from . import OUTPUT_DIR, LIFE_EXPECTANCY_FILE_PATH, SAVE_FILE_PATH
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data()
+    #clean_data()
+    clean_data(LIFE_EXPECTANCY_FILE_PATH,'PT')
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )

2f4c92e90d030ef0e81dd2dce2d9457f63ae4ffc
carolinafm216 <39195248+carolinafm216@users.noreply.github.com>
2023-03-13 11:30:11 +0000

    pylint correction

--- assignments/life_expectancy/tests/__init__.py
+++ assignments/life_expectancy/tests/__init__.py
@@ -1,6 +1,5 @@
 """Test suite for the 1st assignment"""
 from pathlib import Path
-from pathlib import Path
 
 LIFE_EXPECTANCY_FILE_PATH=Path('eu_life_expectancy_raw.tsv')
 #LIFE_EXPECTANCY_FILE_PATH=r"C:\Users\cfmarreiros\Documents\GitHub\FAAST\assignments\life_expectancy\data\eu_life_expectancy_raw.tsv"
